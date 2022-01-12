aa463c29f8b2b667080420275f972c1f07e6679d
patriciamsro <patriciamsro@gmail.com>
2022-01-12 20:56:08 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/example/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,39 @@
+package com.example.cursomc.domain.enums;
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurírica");
+
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod () {
+		return cod;
+	}
+	
+	public String getDescricao () {
+		return descricao;
+	}
+	
+	//ANALISAR MELHOR ESSA PARTE DO CÓDIGO PARA ENTENDER
+	public static TipoCliente toEnum(Integer cod) {
+		
+		if(cod == null) {
+			return null;
+		}
+		
+		for (TipoCliente x : TipoCliente.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
